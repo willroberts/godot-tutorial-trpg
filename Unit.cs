57565a6ff88095023658efafc8b0ac38ef83d877
Will Roberts <willroberts3@protonmail.com>
2023-08-06 11:23:06 +0200

    Adds Unit class

---
+++ Unit.cs
@@ -0,0 +1,123 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+[GlobalClass, Tool]
+public partial class Unit : Path2D
+{
+	[Export]
+	public Grid Grid = (Grid)GD.Load("res://Grid.tres");
+
+	[Export]
+	public int MoveRange = 6;
+
+	[Export]
+	public Texture Skin;
+
+	[Export]
+	public  Vector2 SkinOffset = Vector2.Zero;
+
+	[Export]
+	public float MoveSpeed = (float)600.0;
+
+	[Signal]
+	public delegate void WalkFinishedEventHandler();
+
+	public Vector2 Cell = Vector2.Zero;
+	public bool IsSelected = false;
+	private bool _IsWalking = false;
+	private Sprite2D _Sprite;
+	private AnimationPlayer _AnimPlayer;
+	private PathFollow2D _PathFollow;
+
+	public override void _Ready()
+	{
+		SetProcess(false);
+
+		_PathFollow = GetNode<PathFollow2D>("PathFollow2D");
+		_Sprite = _PathFollow.GetNode<Sprite2D>("Sprite");
+		_AnimPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+
+		Cell = Grid.CalculateGridPosition(Position);
+		Position = Grid.CalculateMapPosition(Cell);
+		
+		if (!Engine.IsEditorHint())
+		{
+			Curve = new Curve2D();
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		_PathFollow.Progress += MoveSpeed * (float)delta;
+		
+		if (_PathFollow.ProgressRatio >= 1.0)
+		{
+			_IsWalking = false;
+			_PathFollow.Progress = 0.0F;
+			Position = Grid.CalculateMapPosition(Cell);
+			Curve.ClearPoints();
+			EmitSignal("walk_finished");
+		}
+	}
+
+	public void SetCell(Vector2 Value)
+	{
+		Cell = Grid.Clamped(Value);
+	}
+
+	public void SetIsSelected(bool Value)
+	{
+		IsSelected = Value;
+		if (IsSelected)
+		{
+			_AnimPlayer.Play("selected");
+		}
+		else
+		{
+			_AnimPlayer.Play("idle");
+		}
+	}
+
+	public async void SetSkin(Texture2D Value)
+	{
+		Skin = Value;
+		if (_Sprite == null)
+		{
+			// Wait until _Ready() is done.
+			await ToSignal(this, "ready");
+		}
+		_Sprite.Texture = Value;
+	}
+
+	public async void SetSkinOffset(Vector2 Value)
+	{
+		SkinOffset = Value;
+		if (_Sprite == null)
+		{
+			// Wait until _Ready() is done.
+			await ToSignal(this, "ready");
+		}
+		_Sprite.Position = Value;
+	}
+
+	public void WalkAlong(Godot.Collections.Array<Vector2> Path)
+	{
+		if (Path.IsEmpty()) { return; }
+		
+		Curve.AddPoint(Vector2.Zero);
+		foreach (Vector2 Point in Path)
+		{
+			Curve.AddPoint(Grid.CalculateMapPosition(Point) - Position);
+		}
+		
+		Cell = Path[-1];
+		_IsWalking = true;
+	}
+
+	private void _SetIsWalking(bool Value)
+	{
+		_IsWalking = Value;
+		SetProcess(_IsWalking);
+	}
+}
