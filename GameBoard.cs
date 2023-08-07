a6841b3ef7ddc9a4a0a01f99f28aab87e064e4db
Will Roberts <willroberts3@protonmail.com>
2023-08-07 17:14:40 +0200

    Adds _FloodFill function

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,6 +1,7 @@
 using Godot;
 using Godot.Collections;
 using System;
+using System.Collections;
 
 [GlobalClass]
 public partial class GameBoard : Node2D
@@ -20,7 +21,7 @@ public partial class GameBoard : Node2D
 		_Reinitialize();
 
 		// Debugging.
-		GD.Print(_Units);
+		//GD.Print(_Units);
 	}
 
 	public override void _Process(double delta) {}
@@ -49,5 +50,45 @@ public partial class GameBoard : Node2D
 			);
 			_Units[Coords] = ThisUnit;
 		}
+	}
+
+	public Array<Vector2I> GetWalkableCells(Unit ThisUnit)
+	{
+		return _FloodFill(
+			new Vector2I(
+				(int)ThisUnit.Cell.X,
+				(int)ThisUnit.Cell.Y
+			),
+			ThisUnit.MoveRange
+		);
+	}
+
+	private Array<Vector2I> _FloodFill(Vector2I Cell, int MaxDistance)
+	{
+		Array<Vector2I> Result = new();
+
+		System.Collections.Generic.Stack<Vector2I> CellStack = new();
+		CellStack.Push(Cell);
+		while (CellStack.Count != 0)
+		{
+			Vector2I CurrentCell = CellStack.Pop();
+			if (!Grid.IsWithinBounds(CurrentCell)) { continue; }
+			if (Result.Contains(CurrentCell)) { continue; }
+
+			Vector2I Difference = (CurrentCell - Cell).Abs();
+			int Distance = Difference.X + Difference.Y;
+			if (Distance > MaxDistance) { continue; }
+
+			Result.Add(CurrentCell);
+			foreach (Vector2I Direction in DIRECTIONS)
+			{
+				Vector2I Coords = CurrentCell + Direction;
+				if (IsOccupied(Coords)) { continue; }
+				if (Result.Contains(Coords)) { continue; }
+				CellStack.Push(Coords);
+			}
+		}
+
+		return Result;
 	}
 }
